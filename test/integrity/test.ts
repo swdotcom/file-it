d4764cb58f80753f729f90a4e3b850f6ba78c427
xavluiz <xavluiz@gmail.com>
2020-06-28 17:25:38 -0700

    added another integrity test

--- test/integrity/test.ts
+++ test/integrity/test.ts
@@ -22,7 +22,24 @@ describe("File Integrity Tests", function () {
     done();
   });
 
-  it("Corrupt a file and recover with backup", function () {
+  it("Test json corrupt using apis", function () {
+    const file = path.join(TEST_DIR, 'tmp.json');
+    const jsonData = { integrity: "test" };
+    // save a valid json
+    fileIt.writeJsonFileSync(file, jsonData);
+    let val = fileIt.getJsonValue(file, "integrity");
+    expect(val).to.equal("test");
+
+    // attemp to corrupt file using api
+    const result = fileIt.writeContentFileSync(file, "{integrity}:test");
+    expect(result.error).to.not.be.null;
+
+    // the json object should still be valid
+    val = fileIt.getJsonValue(file, "integrity");
+    expect(val).to.equal("test");
+  });
+
+  it("Test json corrupt using native functions", function () {
     const file = path.join(TEST_DIR, 'tmp.json');
     const jsonData = { integrity: "test" };
     // save a valid json
@@ -31,10 +48,15 @@ describe("File Integrity Tests", function () {
     expect(val).to.equal("test");
 
     // corrupt it
-    const result = fileIt.writeContentFileSync(file, "{integrity}:test");
-    expect(result.error).to.not.be.null;
+    fs.writeFileSync(file, "{integrety}:test", {
+      encoding: "utf8",
+    });
+    const corruptedFile = fs.readFileSync(file, {
+      encoding: "utf8",
+    });
+    expect(corruptedFile.includes(":test")).to.be.true;
 
-    // the json object should still be valid
+    // the json object should still be valid based on the cached content
     val = fileIt.getJsonValue(file, "integrity");
     expect(val).to.equal("test");
   });
