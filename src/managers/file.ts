f6a0d2d028c5b87e28244e340c2dbddf329f18b5
Xavier Luiz <xavluiz@gmail.com>
2021-05-06 16:38:08 -0700

    comments update five

--- src/managers/file.ts
+++ src/managers/file.ts
@@ -2,7 +2,7 @@ import { cleanJsonString, jsonStringify } from "./util";
 import fs = require("fs");
 import universalify = require("universalify");
 
-// fileName => jsonData
+// fileName => jsonDatalkjsdflkjsdflkjsdflkjsdflkjsdflkjsdflkjsdflkj
 // in case reading ends of with a corrupted object
 const contentMap: any = {};
 
